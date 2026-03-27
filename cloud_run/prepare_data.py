0c2c5d22a2e089ccbbea0723b1754bbc67de71ba
Szymon Fras <szymon.fras13@gmail.com>
2026-03-27 20:11:22 +0100

    Initial commit: BigQuery tables, views and Cloud Run job

--- cloud_run/prepare_data.py
+++
@@ -1,8 +0,0 @@
-import api_strava as strava
-import bigquery_functions as bq
-from config import strava_keys
-def prepare_data():
-    CLIENT_ID,CLIENT_SECRET,REFRESH_TOKEN = strava_keys()
-    token = strava.get_access_token(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
-    df = strava.load_data(token,after_date=bq.get_date())
-    return df
--- datasets/strava/views/v_monthly_avg_kudos.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  DATE_TRUNC(DATE(start_date_local), MONTH) AS month,
-  round(AVG(kudos_count)) AS avg_kudos
-FROM `project-bedff666-ec4c-42c0-ba7.strava.activities_v1`
-GROUP BY month
-ORDER BY month
