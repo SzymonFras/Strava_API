99802aeba294006527ee6f69100de6aca43a55bd
SzymonFras <75011018+SzymonFras@users.noreply.github.com>
2026-05-16 10:15:41 +0200

    Update main.py

--- cloud_run/main.py
+++ cloud_run/main.py
@@ -1,15 +1,8 @@
 from prepare_data import prepare_data
 import bigquery_functions as bq
 def sync_strava_activities(request):
-    """
-        Cloud Function: pobiera nowe aktywności ze Stravy od ostatniego start_date
-        w BigQuery i wstawia je do głównej tabeli bez duplikatów.
-        """
-    # 1. Download new activities
     try:
         df_new = prepare_data()
-
-    # 2. Jeśli są nowe wiersze, wrzuć do BigQuery
         if not df_new.empty:
             print(f"Wstawiam {len(df_new)} wierszy do BQ")
             bq.upsert_activities(df_new)
